ec21028408ebcbf6b120ba9749a9de84a3455b5a
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-08-09 04:58:46 +0900

    test: 테스트코드 공통로직 분리 및 소프트코딩 일부 적용

--- cypress/integration/counter.spec.js
+++ cypress/integration/counter.spec.js
@@ -1,37 +1,46 @@
+const SELECTOR = Object.freeze({
+  PLUS_BUTTON: '.plus-button',
+  MINUS_BUTTON: '.minus-button',
+  COUNTER: '.count-display',
+});
+
 describe('ui-counter', () => {
   beforeEach(() => {
     cy.visit('http://127.0.0.1:8000/');
   });
 
+  const clickButton = (selector, times = 1) => {
+    for (let i = 0; i < times; i++) cy.get(selector).click();
+  };
+
+  const counterShouldBe = (expected) =>
+    cy.get(SELECTOR.COUNTER).should('have.value', expected);
+
   it('생성시 버튼과 초기값(10)을 렌더링 한다', () => {
-    cy.get('.counter').find('.count-display').should('have.value', '10');
+    counterShouldBe('10');
   });
 
   it('+ 버튼을 클릭 시 count가 1증가한다', () => {
-    cy.get('plus-button').click();
-    cy.get('.count-display').should('have.value', '11');
+    clickButton(SELECTOR.PLUS_BUTTON);
+    counterShouldBe('11');
   });
 
   it('- 버튼을 클릭 시 count가 1감소한다', () => {
-    cy.get('minus-button').click();
-    cy.get('.count-display').should('have.value', '9');
+    clickButton(SELECTOR.MINUS_BUTTON);
+    counterShouldBe('9');
   });
 
   it('count의 값은 12를 넘어갈 수 없다', () => {
-    const TOTAL_CLICK = 5;
-    for (let clickCount = 0; clickCount < TOTAL_CLICK; clickCount++) {
-      cy.get('plus-button').click();
-    }
+    const NUMBER_OF_CLICKS = 5;
+    clickButton(SELECTOR.PLUS_BUTTON, NUMBER_OF_CLICKS);
 
-    cy.get('.count-display').should('have.value', 12);
+    counterShouldBe('12');
   });
 
   it('count의 값은 8 아래로 떨어질 수 없다', () => {
-    const TOTAL_CLICK = 5;
-    for (let clickCount = 0; clickCount < TOTAL_CLICK; clickCount++) {
-      cy.get('minus-button').click();
-    }
+    const NUMBER_OF_CLICKS = 5;
+    clickButton(SELECTOR.MINUS_BUTTON, NUMBER_OF_CLICKS);
 
-    cy.get('.count-display').should('have.value', 8);
+    counterShouldBe('8');
   });
 });
