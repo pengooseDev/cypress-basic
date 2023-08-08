0da0e3a5f71cc4accc175766ff56d42303417b48
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-08-09 04:36:13 +0900

    test: TC추가(+ 버튼을 클릭 시 count가 1증가한다)

--- cypress/integration/counter.spec.js
+++ cypress/integration/counter.spec.js
@@ -6,4 +6,9 @@ describe('ui-counter', () => {
   it('생성시 버튼과 초기값(10)을 렌더링 한다', () => {
     cy.get('.counter').find('.count-display').should('have.value', '10');
   });
+
+  it('+ 버튼을 클릭 시 count가 1증가한다', () => {
+    cy.get('plus-button').click();
+    cy.get('.count-display').should('have.value', '11');
+  });
 });
